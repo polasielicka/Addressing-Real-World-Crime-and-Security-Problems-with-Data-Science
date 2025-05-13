62e3ba64036b5fe7e4305031fd7d41fab8350ff1
IanOssenblok <ian@ianossenblok.com>
2025-05-13 13:27:25 +0200

    ward function + removes rows without location

--- Ian/add_ward.py
+++ Ian/add_ward.py
@@ -2,50 +2,65 @@ import os
 import glob
 import pandas as pd
 
-combined_data = glob.glob(os.path.join("../data_CBL", "*", "*-street.csv"))
-df_lsoa = pd.concat((pd.read_csv(f) for f in combined_data), ignore_index=True)
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-shapefile_path = os.path.join(script_dir, '..', "Ian", "LSOA_(2021)_to_Electoral_Ward_(2024)_to_LAD_(2024)_Best_Fit_Lookup_in_EW.csv")
-lsoa_lookup = pd.read_csv(shapefile_path)
+def load_burglary_data():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
 
-shapefile_path2 = os.path.join(script_dir, '..', "Ian", "Lower_Layer_Super_Output_Area_(2011)_to_Ward_(2018)_Lookup_in_England_and_Wales_v3.csv")
-lsoa_lookup2 = pd.read_csv(shapefile_path2)
+    data_dir = os.path.join(script_dir, '..', 'data_CBL')
+    lookup_path_2024 = os.path.join(script_dir, '..', 'Ian',
+                                    'LSOA_(2021)_to_Electoral_Ward_(2024)_to_LAD_(2024)_Best_Fit_Lookup_in_EW.csv')
+    lookup_path_2018 = os.path.join(script_dir, '..', 'Ian',
+                                    'Lower_Layer_Super_Output_Area_(2011)_to_Ward_(2018)_Lookup_in_England_and_Wales_v3.csv')
 
-print(lsoa_lookup.head())
+    # Load crime data
+    combined_data = glob.glob(os.path.join(data_dir, "*", "*-street.csv"))
+    df_lsoa = pd.concat((pd.read_csv(f) for f in combined_data), ignore_index=True)
 
-print(lsoa_lookup2.head())
+    # Load lookup tables
+    lsoa_lookup = pd.read_csv(lookup_path_2024)
+    lsoa_lookup2 = pd.read_csv(lookup_path_2018)
 
-df1 = df_lsoa.merge(
-    lsoa_lookup[['LSOA21CD', 'WD24NM']],
-    how='left',
-    left_on='LSOA code',
-    right_on='LSOA21CD'
-)
-df1.rename(columns={'WD24NM': 'ward2024'}, inplace=True)
-df1.drop(columns=['LSOA21CD'], inplace=True)
+    df1 = df_lsoa.merge(
+        lsoa_lookup[['LSOA21CD', 'WD24NM']],
+        how='left',
+        left_on='LSOA code',
+        right_on='LSOA21CD'
+    ).rename(columns={'WD24NM': 'ward2024'}).drop(columns=['LSOA21CD'])
 
-df = df1.merge(
-    lsoa_lookup2[['LSOA11CD', 'WD18NM']],
-    how='left',
-    left_on='LSOA code',
-    right_on='LSOA11CD'
-)
-df.rename(columns={'WD18NM': 'ward2018'}, inplace=True)
-df.drop(columns=['LSOA11CD'], inplace=True)
+    df = df1.merge(
+        lsoa_lookup2[['LSOA11CD', 'WD18NM']],
+        how='left',
+        left_on='LSOA code',
+        right_on='LSOA11CD'
+    ).rename(columns={'WD18NM': 'ward2018'}).drop(columns=['LSOA11CD'])
 
-print(df.columns)
-df['ward'] = df['ward2024'].fillna(df['ward2018'])
+    # Combine ward information
+    df['ward'] = df['ward2024'].fillna(df['ward2018'])
 
-burglary_df = df[df["Crime type"].str.lower() == "burglary"]
+    # Filter for burglaries
+    burglary_df = df[df["Crime type"].str.lower() == "burglary"]
 
-print(burglary_df.head())
-print(burglary_df.columns)
+    # removes rows that dont have location data
+    initial_row_count = len(burglary_df)
+    burglary_df = burglary_df[burglary_df['LSOA code'].notna()]
+    removed_rows = initial_row_count - len(burglary_df)
+    print(f"Removed {removed_rows} rows with missing 'LSOA code'.")
 
-missing_ward_lsoas = burglary_df[burglary_df['ward'].isnull()]['Crime ID']
-print(missing_ward_lsoas)
+    return burglary_df
 
-# row = df[df['Crime ID'] == '70e8e6ddbf8ee26c8e38997a45450b6bebf12f634ab799']
-# print(row)
-#
-#
+
+# every row that doesnt have missing location data has a ward entry
+df = load_burglary_data()
+missing_wards = df[df['ward'].isnull()]
+print(len(missing_wards))
+
+# there are 2977 rows that dont have location data
+missing_counts = df.isna().sum()
+print(missing_counts)
+
+# the location value for these is "No Location"
+unique_locations = df[df['LSOA code'].isna()]['Location'].dropna().unique()
+print(unique_locations)
+
+# 162358 total burglaries
+print(len(df))
