2af0b8d76cb2afc0e8fd9bdf7ca8a1771632e5d4
IanOssenblok <ian@ianossenblok.com>
2025-06-02 01:03:58 +0200

    IMD to ward 2025

--- Ian/dashboard.py
+++ Ian/dashboard.py
@@ -6,7 +6,7 @@ from dash import dcc, html, Input, Output
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-shapefile_path = os.path.join(script_dir, "..", "data", "coordinate_mapping_2025", "london_only_wards_2025.shp")
+shapefile_path = os.path.join(script_dir, "..", "data", "coordinate_mapping_2025", "london_wards_area_weighted_IMD_CORRECTED.shp")
 gdf = gpd.read_file(shapefile_path)
 gdf = gdf.to_crs(epsg=4326)
 
@@ -22,7 +22,7 @@ fig = px.choropleth_mapbox(
     center={"lat": 51.5074, "lon": -0.1278},
     opacity=0.3,
     hover_name="NAME",
-    hover_data=["HECTARES"]
+    hover_data=["HECTARES", "weighted_I", "weighted_1"]
 )
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
