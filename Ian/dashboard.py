dd9df57f21a1683979f64596e20a7820b2814d9b
20230931 <p.k.sielicka@student.tue.nl>
2025-06-01 09:11:49 +0200

    sarima per ward

--- Ian/dashboard.py
+++ Ian/dashboard.py
@@ -1,22 +1,14 @@
 import geopandas as gpd
-import pandas as pd
 import json
 import plotly.express as px
 import dash
 from dash import dcc, html, Input, Output
 import os
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.width', None)
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
-shapefile_path = os.path.join(script_dir, "..", "data", "coordinate_mapping_2025", "london_only_wards_2025.shp")
+shapefile_path = os.path.join(script_dir, "..", "data_CBL", "coordinate_mapping_2018", "London_Ward.shp")
 gdf = gpd.read_file(shapefile_path)
 gdf = gdf.to_crs(epsg=4326)
-# print(len(london_wards))
-# print(london_wards.head())
-
 
 geojson_data = json.loads(gdf.to_json())
 
@@ -30,7 +22,7 @@ fig = px.choropleth_mapbox(
     center={"lat": 51.5074, "lon": -0.1278},
     opacity=0.3,
     hover_name="NAME",
-    hover_data=["HECTARES"]
+    hover_data=["DISTRICT", "HECTARES"]
 )
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
@@ -51,7 +43,7 @@ def display_click_data(clickData):
     if clickData:
         idx = clickData["points"][0]["location"]
         ward = gdf.iloc[idx]
-        return f"Clicked Ward: {ward['NAME']}"
+        return f"Clicked Ward: {ward['NAME']} ({ward['DISTRICT']})"
     return "Placeholder, click on ward to show its name"
 
 if __name__ == "__main__":
