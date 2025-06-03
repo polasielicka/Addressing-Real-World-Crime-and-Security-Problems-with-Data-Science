649d5d1e19570c98d37661996ff1af31ba9da476
IanOssenblok <ian@ianossenblok.com>
2025-06-03 03:34:56 +0200

    sarima coordinate mapping

--- Pola/sarima_per_ward.py
+++ Pola/sarima_per_ward.py
@@ -9,6 +9,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
+from Ian.add_ward import coordinate_mapping
+
 try:
     from pmdarima import auto_arima
 except ImportError as err:
@@ -24,45 +26,8 @@ FUTURE_PERIODS = 12
 SEASONAL_PERIOD = 12
 
 
-def lsoa_mapping():
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(script_dir, '..', 'data_all')  # <-- corrected path
-    combined_data = glob.glob(os.path.join(data_dir, '**', '*-street.csv'), recursive=True)
-
-    if not combined_data:
-        raise FileNotFoundError(f"No CSVs found in {data_dir}")
-
-    lookup_path_2024 = os.path.join(script_dir, '..', 'data', 'best_fit_lsoa_data (dont use)',
-                                    'LSOA_(2021)_to_Electoral_Ward_(2024)_to_LAD_(2024)_Best_Fit_Lookup_in_EW.csv')
-    lookup_path_2018 = os.path.join(script_dir, '..', 'data', 'best_fit_lsoa_data (dont use)',
-                                    'Lower_Layer_Super_Output_Area_(2011)_to_Ward_(2018)_Lookup_in_England_and_Wales_v3.csv')
-
-    df_lsoa = pd.concat((pd.read_csv(f) for f in combined_data), ignore_index=True)
-
-    lsoa_lookup = pd.read_csv(lookup_path_2024)
-    lsoa_lookup2 = pd.read_csv(lookup_path_2018)
-
-    df1 = df_lsoa.merge(
-        lsoa_lookup[['LSOA21CD', 'WD24NM']],
-        how='left',
-        left_on='LSOA code',
-        right_on='LSOA21CD'
-    ).rename(columns={'WD24NM': 'ward2024'}).drop(columns=['LSOA21CD'])
-
-    df = df1.merge(
-        lsoa_lookup2[['LSOA11CD', 'WD18NM']],
-        how='left',
-        left_on='LSOA code',
-        right_on='LSOA11CD'
-    ).rename(columns={'WD18NM': 'ward2018'}).drop(columns=['LSOA11CD'])
-
-    df['ward_name'] = df['ward2024'].fillna(df['ward2018'])
-    burglary_df = df[df["Crime type"].str.lower() == "burglary"]
-    return burglary_df[burglary_df['LSOA code'].notna()]
-
-
 def load_burglary_counts_by_ward() -> pd.DataFrame:
-    df = lsoa_mapping()
+    df = coordinate_mapping()
     df["Month"] = pd.to_datetime(df["Month"], format="%Y-%m")
     grouped = (
         df.groupby(["ward_name", "Month"])
